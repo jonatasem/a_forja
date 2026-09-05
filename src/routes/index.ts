3ba8bf2b87ca98a0f70b295c9563c26c2c0d17ca
jonatasem <jonatas.em20@gmail.com>
2026-09-05 17:05:23 -0300

    feat: inicialização do projeto fastify com dotenv e cors

---
+++ src/routes/index.ts
@@ -0,0 +1,16 @@
+import type {
+  FastifyInstance,
+  FastifyRequest,
+  FastifyReply,
+} from "fastify";
+
+export async function routes(
+  fastify: FastifyInstance,
+) {
+  fastify.get(
+    "/teste",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      return {ok: "ok"};
+    },
+  );
+}
---
+++ src/server.ts
@@ -0,0 +1,46 @@
+import fastify from "fastify";
+import cors from "@fastify/cors";
+import { routes } from "./routes/index.js";
+import dotenv from "dotenv";
+
+const app = fastify({
+  logger: false,
+});
+
+// Carrega as variáveis do arquivo .env para o process.env
+dotenv.config();
+
+// Variavel de ambiente para o frontend
+const urlDevelop = process.env.URL_DEVELOP;
+const port = process.env.PORT;
+
+if (!urlDevelop) {
+  throw new Error("Informe a url do frontend.");
+}
+
+if (!port) {
+  throw new Error("Informe uma porta para o backend.");
+}
+
+const start = async () => {
+  await app.register(cors, {
+    origin: urlDevelop,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  });
+
+  await app.register(routes);
+
+  try {
+    await app.listen({
+      port: Number(port),
+      host: "0.0.0.0", // Permite que o servidor seja acessado de qualquer endereço IP
+    });
+    console.log(`Server is running on port ${port}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+start();
